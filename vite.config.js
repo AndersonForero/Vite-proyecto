b08fd0249d3c581eb15848e94c567c591665050a
Anderson Forero <leidyusernameuwu@gmail.com>
2025-01-04 01:05:40 -0430

    Corrigiendo base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://AndersonForero.github.io/vite-project"
+  base: "https://andersonforero.github.io/Vite-proyecto"
 })
